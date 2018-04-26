3183ae8b979915d72a304139697b72d6b239726f
chisom5 <okoyechisom54@gmail.com>
2018-04-26 15:56:54 +0100

    moviesApp done in VanillaJS

---
+++ README.md
@@ -0,0 +1,9 @@
+# This is a Movie Application, implemented in VanillaJS
+this application access movie data from omdbAPI.
+it has two html pages one that display all the movies data gotten from the api
+and the other page that display the movies details when clicked.
+
+## things remain to be done here are:
+
+* The second page known as movie.html.
+and for now this project can be viewed by this link 
---
+++ js/main.js
@@ -0,0 +1,37 @@
+document.querySelector('#searchForm').addEventListener('submit', getMovies);
+
+function getMovies(e) {
+
+    e.preventDefault();
+    //get user input
+    let searchInput = document.querySelector('#searchTxt').value;
+
+    //fetch data from the Api 
+    axios.get('http://www.omdbapi.com/?s=' + searchInput + '&apikey=d5d3093e')
+        .then((response) => {
+
+            let movies = response.data.Search;
+
+            let output = '';
+            for (var i = 0; i < movies.length; i++) {
+                var Poster = movies[i].Poster;
+                var Title = movies[i].Title;
+                output += `
+                <div class="col-md-3">
+                    <div class="well text-center">
+                    <img src="` + Poster + `">
+                    <h5>` + Title + `</h5>
+                    <a onClick="moviesSelected()" class="btn btn-primary" href="">Movies Details</a>
+                    </div>
+                </div>
+                `;
+            }
+
+            document.querySelector('#movies').innerHTML = output;
+        })
+
+    .catch((err) => {
+        console.log(err);
+    });
+
+}
